9efd86f8a28a5b0be310dcf8c0be34ed40745a6c
haazime <h4zime@gmail.com>
2014-10-11 20:58:23 +0900

    refactor step1

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -5,11 +5,17 @@ class VendingMachine
   end
 
   def receive(money)
-    return money if [1, 5, 5000, 10000].include?(money)
+    return money if available_money?(money)
     @money_collection << money
   end
 
   def pay_back
     @money_collection.inject(&:+)
+  end
+
+private
+
+  def available_money?(money)
+    [1, 5, 5000, 10000].include?(money)
   end
 end
