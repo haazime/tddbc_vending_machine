435d3ea608d9ee512ff388546534aebd5d986334
haazime <h4zime@gmail.com>
2014-10-12 11:35:40 +0900

    refactor

--- spec/vending_machine/serve_drink_spec.rb
+++ spec/vending_machine/serve_drink_spec.rb
@@ -10,68 +10,24 @@ RSpec.describe VendingMachine do
   end
 
   context "コーラの在庫がある場合" do
-    context "120円を投入した時" do
-      before do
-        [100, 10, 10].each {|m| machine.receive_money(m) }
-      end
+    [
+      { money: [100, 10, 10], pay_back: 0, sales: 120 },
+      { money: [100, 10, 10, 10], pay_back: 10, sales: 120 },
+      { money: [100, 50], pay_back: 30, sales: 120 },
+      { money: [500], pay_back: 380, sales: 120 },
+      { money: [1000], pay_back: 880, sales: 120 },
+    ].each do |c|
+      context "#{c[:money].inject(&:+)}円を投入した時" do
+        before do
+          c[:money].each {|m| machine.receive_money(m) }
+        end
 
-      it do
-        expect { subject }.to change { machine.drinks[:quantity] }
-          .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
-        expect(machine.pay_back).to eq(0)
-        expect(machine.sales).to eq(120)
-      end
-    end
-
-    context "130円を投入した時" do
-      before do
-        [100, 10, 10, 10].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to change { machine.drinks[:quantity] }
-          .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
-        expect(machine.pay_back).to eq(10)
-        expect(machine.sales).to eq(120)
-      end
-    end
-
-    context "150円を投入した時" do
-      before do
-        [100, 50].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to change { machine.drinks[:quantity] }
-          .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
-        expect(machine.pay_back).to eq(30)
-        expect(machine.sales).to eq(120)
-      end
-    end
-
-    context "500円を投入した時" do
-      before do
-        [500].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to change { machine.drinks[:quantity] }
-          .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
-        expect(machine.pay_back).to eq(380)
-        expect(machine.sales).to eq(120)
-      end
-    end
-
-    context "1000円を投入した時" do
-      before do
-        [1000].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to change { machine.drinks[:quantity] }
-          .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
-        expect(machine.pay_back).to eq(880)
-        expect(machine.sales).to eq(120)
+        it do
+          expect { subject }.to change { machine.drinks[:quantity] }
+            .from(machine.drinks[:quantity]).to(machine.drinks[:quantity] - 1)
+          expect(machine.pay_back).to eq(c[:pay_back])
+          expect(machine.sales).to eq(c[:sales])
+        end
       end
     end
 
@@ -93,63 +49,23 @@ RSpec.describe VendingMachine do
       described_class.new(name: 'コーラ', price: 120, quantity: 0)
     end
 
-    context "120円を投入した時" do
-      before do
-        [100, 10, 10].each {|m| machine.receive_money(m) }
-      end
+    [
+      { money: [100, 10, 10], pay_back: 120, sales: 0 },
+      { money: [100, 10, 10, 10], pay_back: 130, sales: 0 },
+      { money: [100, 50], pay_back: 150, sales: 0 },
+      { money: [500], pay_back: 500, sales: 0 },
+      { money: [1000], pay_back: 1000, sales: 0 },
+    ].each do |c|
+      context "#{c[:money].inject(&:+)}円を投入した時" do
+        before do
+          c[:money].each {|m| machine.receive_money(m) }
+        end
 
-      it do
-        expect { subject }.to_not change { machine.drinks[:quantity] }
-        expect(machine.pay_back).to eq(120)
-        expect(machine.sales).to eq(0)
-      end
-    end
-
-    context "130円を投入した時" do
-      before do
-        [100, 10, 10, 10].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to_not change { machine.drinks[:quantity] }
-        expect(machine.pay_back).to eq(130)
-        expect(machine.sales).to eq(0)
-      end
-    end
-
-    context "150円を投入した時" do
-      before do
-        [100, 50].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to_not change { machine.drinks[:quantity] }
-        expect(machine.pay_back).to eq(150)
-        expect(machine.sales).to eq(0)
-      end
-    end
-
-    context "500円を投入した時" do
-      before do
-        [500].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to_not change { machine.drinks[:quantity] }
-        expect(machine.pay_back).to eq(500)
-        expect(machine.sales).to eq(0)
-      end
-    end
-
-    context "1000円を投入した時" do
-      before do
-        [1000].each {|m| machine.receive_money(m) }
-      end
-
-      it do
-        expect { subject }.to_not change { machine.drinks[:quantity] }
-        expect(machine.pay_back).to eq(1000)
-        expect(machine.sales).to eq(0)
+        it do
+          expect { subject }.to_not change { machine.drinks[:quantity] }
+          expect(machine.pay_back).to eq(c[:pay_back])
+          expect(machine.sales).to eq(c[:sales])
+        end
       end
     end
   end
